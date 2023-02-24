beea9ec4986f90308ef5aeec3ef8ebade0ee3efb
EJ <yijie@easyship.com>
2023-02-24 19:06:37 +0800

    feat/monitor-git: Fix TypeError in monitor_git_staging.rb caused by using File.read instead of File.readlines

--- monitor_git_staging.rb
+++ monitor_git_staging.rb
@@ -5,17 +5,10 @@ if !Dir.exist?(".git")
 end
 
 git = Git.open(".")
-files = git.status.added
-
-if files.empty?
-  raise "No files in staging!"
-end
-
-files.each do |file|
-  content = File.read(file)
-  str = content[0..3999]
-  first_line = str.lines.first
-  last_line = str.lines.last
+diffs = git.diff.each do |diff|
+  patch = diff.patch
+  first_line = patch.lines.first
+  last_line = patch.lines.last
 
   puts "First line: #{first_line}"
   puts "Last line: #{last_line}"
