2afd583aa39d4938c56323d389b6326bc3eeea36
EJ (YiJie) <yijie@easyship.com>
2023-03-24 22:00:53 +0800

    ci: update-release-script (#28)

--- push_release_tag.rb
+++ push_release_tag.rb
@@ -1,14 +1,11 @@
-require_relative "lib/aicommit/version"
-
-release = Aicommit::VERSION
-
 cmd = <<-BASH
   git checkout develop
   git fetch && git reset --hard origin/develop
+  export release=$(ruby -e "require_relative 'lib/aicommit/version'; puts Aicommit::VERSION")
 
-  git tag #{release}
-  git push origin #{release}
-  git push origin #{release}:master --force-with-lease
+  git tag ${release}
+  git push origin ${release}
+  git push origin ${release}:master --force-with-lease
 BASH
 
 system(cmd)
