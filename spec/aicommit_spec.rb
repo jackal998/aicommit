88457ee75bae142a97bf4c9c56a69c58ef1ec7bc
EJ (YiJie) <yijie@easyship.com>
2024-01-06 00:17:17 +0800

    feat: revamp Aicommit with selected model (#37)

--- spec/aicommit_spec.rb
+++ spec/aicommit_spec.rb
@@ -1,127 +1,61 @@
 require "aicommit"
 
 describe Aicommit do
+  let(:ai_client) { instance_double(AiClient) }
   let(:git_client) { instance_double(GitClient) }
-  let(:commit_message_generator) { instance_double(CommitMessageGenerator) }
-  let(:token_manager) { instance_double(TokenManager) }
 
   before do
+    allow(AiClient).to receive(:new).and_return(ai_client)
     allow(GitClient).to receive(:new).and_return(git_client)
-    allow(CommitMessageGenerator).to receive(:new).and_return(commit_message_generator)
-    allow(TokenManager).to receive(:new).and_return(token_manager)
   end
 
   describe "#run" do
-    context "when no response from OpenAI" do
-      before do
-        allow(token_manager).to receive(:fetch).with("OPENAI_API_TOKEN").and_return("VALID_API_KEY")
-      end
+    before { allow(git_client).to receive(:git_diff_str).and_return("diff") }
 
-      it "deplays error message and exit" do
-        expect(git_client).to receive(:git_diff_str).and_return("diff")
+    it "displays commit options and processes user choice" do
+      expect(ai_client).to receive(:chat).with("diff").and_return("generated commit message")
+      allow(subject).to receive(:gets).and_return("Y\n")
 
-        expect(CommitMessageGenerator).to receive(:new).with("VALID_API_KEY").and_return(commit_message_generator)
-        expect(commit_message_generator).to receive(:generate).with("diff").and_return({code: 500, result: "Net::ReadTimeout"})
+      expect(git_client).to receive(:commit_all).with("generated commit message")
+      expect { subject.run }.to output(/Do you want to keep this commit_message?/).to_stdout.and(raise_error(SystemExit))
+    end
 
-        expect { subject.run }.to output(/OpenAI connection timeout./).to_stdout.and(raise_error(SystemExit))
+    context "when user decides to regenerate the message (R)" do
+      it "regenerates commit message" do
+        expect(ai_client).to receive(:chat).with("diff").exactly(2).times.and_return("generated commit message", "newly generated commit message")
+        allow(subject).to receive(:gets).and_return("R\n", "Y\n")
+
+        expect(git_client).to receive(:commit_all).with("newly generated commit message")
+        expect { subject.run }.to output(/Regenerating/).to_stdout.and(raise_error(SystemExit))
       end
     end
 
-    context "when invalid api token" do
-      before do
-        allow(token_manager).to receive(:fetch).with("OPENAI_API_TOKEN").and_return("INVALID_API_KEY", "VALID_API_KEY")
-      end
+    context "when user decides to enter a new message (N)" do
+      it "allows user to overwrite commit message" do
+        expect(ai_client).to receive(:chat).with("diff").and_return("generated commit message")
+        allow(subject).to receive(:gets).and_return("N\n", "user entered message\n", "Y\n")
 
-      it "asks for valid api token and save" do
-        expect(git_client).to receive(:git_diff_str).and_return("diff")
-
-        expect(CommitMessageGenerator).to receive(:new).with("INVALID_API_KEY").and_return(commit_message_generator)
-        expect(CommitMessageGenerator).to receive(:new).with("VALID_API_KEY").and_return(commit_message_generator)
-
-        expect(commit_message_generator).to receive(:generate).with("diff").and_return({code: 401, result: ""}, {code: 200, result: "commit message"})
-        expect(token_manager).to receive(:write!).with("OPENAI_API_TOKEN")
-
-        allow(subject).to receive(:gets).and_return("VALID_API_KEY\n", "Y\n")
-        expect(git_client).to receive(:commit_all).with("commit message")
-
-        expect { subject.run }.to output(/Invalid API key./).to_stdout.and(raise_error(SystemExit))
+        expect(git_client).to receive(:commit_all).with("user entered message")
+        expect { subject.run }.to output(/Please enter your new commit_message/).to_stdout.and(raise_error(SystemExit))
       end
     end
 
-    context "when valid api token" do
-      before do
-        allow(token_manager).to receive(:fetch).with("OPENAI_API_TOKEN").and_return("API_KEY")
+    context "when user quits (Q)" do
+      it "exits without committing" do
+        expect(ai_client).to receive(:chat).with("diff").and_return("generated commit message")
+        allow(subject).to receive(:gets).and_return("Q\n")
+
+        expect { subject.run }.to output(/Quit without committing/).to_stdout.and(raise_error(SystemExit))
       end
+    end
 
-      it "shows hint commit message and ask user if commit or not" do
-        expect(git_client).to receive(:git_diff_str).and_return("diff")
+    context "when input is not Y, R, N, Q" do
+      it "shows warning message and loops again" do
+        expect(ai_client).to receive(:chat).with("diff").and_return("generated commit message")
+        allow(subject).to receive(:gets).and_return("invalid\n", "Y\n")
 
-        expect(commit_message_generator).to receive(:generate).with("diff").and_return({code: 200, result: "commit message"})
-
-        allow(subject).to receive(:gets).and_return("Y\n")
-        expect(git_client).to receive(:commit_all).with("commit message")
-
-        expect { subject.run }.to output(/commit message/).to_stdout.and(raise_error(SystemExit))
-      end
-
-      context "when input is Y" do
-        it "tells user what commit message would be then commit and exit program" do
-          expect(git_client).to receive(:git_diff_str).and_return("diff")
-
-          expect(commit_message_generator).to receive(:generate).with("diff").and_return({code: 200, result: "commit message"})
-
-          allow(subject).to receive(:gets).and_return("Y\n")
-          expect(git_client).to receive(:commit_all).with("commit message")
-
-          expect { subject.run }.to output(/All changes have been successfully committed./).to_stdout.and(raise_error(SystemExit))
-        end
-      end
-
-      context "when input is R" do
-        it "regenerates commit message" do
-          expect(git_client).to receive(:git_diff_str).and_return("diff")
-          expect(commit_message_generator).to receive(:generate).with("diff").and_return({code: 200, result: "commit message"}, {code: 200, result: "new commit message"})
-
-          allow(subject).to receive(:gets).and_return("R\n", "Y\n")
-          expect(git_client).to receive(:commit_all).with("new commit message")
-
-          expect { subject.run }.to output(/new commit message/).to_stdout.and(raise_error(SystemExit))
-        end
-      end
-
-      context "when input is N" do
-        it "allows user to overwrite commit message" do
-          expect(git_client).to receive(:git_diff_str).and_return("diff")
-          expect(commit_message_generator).to receive(:generate).with("diff").and_return({code: 200, result: "commit message"})
-
-          allow(subject).to receive(:gets).and_return("N\n", "new commit message\n", "Y\n")
-          expect(git_client).to receive(:commit_all).with("new commit message")
-
-          expect { subject.run }.to output(/new commit message/).to_stdout.and(raise_error(SystemExit))
-        end
-      end
-
-      context "when input is Q" do
-        it "quits without commit" do
-          expect(git_client).to receive(:git_diff_str).and_return("diff")
-          expect(commit_message_generator).to receive(:generate).with("diff").and_return({code: 200, result: "commit message"})
-
-          allow(subject).to receive(:gets).and_return("Q\n")
-
-          expect { subject.run }.to output(/Quit without committing/).to_stdout.and(raise_error(SystemExit))
-        end
-      end
-
-      context "when input is not Y, R, N, Q" do
-        it "shows warning message and loops again" do
-          expect(git_client).to receive(:git_diff_str).and_return("diff")
-
-          allow(subject).to receive(:gets).and_return("invalid\n", "Y\n")
-          expect(commit_message_generator).to receive(:generate).with("diff").and_return({code: 200, result: "commit message"})
-          expect(git_client).to receive(:commit_all).with("commit message")
-
-          expect { subject.run }.to output(/Invalid command./).to_stdout.and(raise_error(SystemExit))
-        end
+        expect(git_client).to receive(:commit_all).with("generated commit message")
+        expect { subject.run }.to output(/Invalid command/).to_stdout.and(raise_error(SystemExit))
       end
     end
   end
